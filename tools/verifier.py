579ba9bbef5fbe97fe9cb2523197954a7dbe081a
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2022-12-11 01:41:57 +0100

    Fixes
    - Removed old non used classes and operators
    - Corrected some issues with verifier

--- tools/verifier.py
+++ tools/verifier.py
@@ -19,7 +19,7 @@
 # Test the whole scene to check if there is any issues in the setup before export to i3d
 
 import bpy
-
+from mathutils import Vector
 
 class I3DEA_OT_verify_scene(bpy.types.Operator):
     bl_idname = "i3dea.verify_scene"
@@ -32,6 +32,7 @@ class I3DEA_OT_verify_scene(bpy.types.Operator):
         error_count = 0
         warning_count = 0
         info_count = 0
+        poly_count = 0
 
         for obj in bpy.data.objects:
             if "placeable" in obj.name.lower():
@@ -54,26 +55,25 @@ class I3DEA_OT_verify_scene(bpy.types.Operator):
                     for _ in children_objs:
                         continue
 
-                print(children_objs)
             if obj.type != 'MESH':
                 continue
             objs = obj.evaluated_get(dg)
             mesh = objs.to_mesh()
-            poly_count = len(mesh.polygons)
+            poly_count += len(mesh.polygons)
 
-            if poly_count > 0 and ('I3D_nonRenderable' and 'I3D_mergeGroup' in obj) and (obj['I3D_nonRenderable'] and obj['I3D_mergeGroup'] == 0):
-                print("Multiple shapes defined!")
+            # if poly_count > 0 and ('I3D_nonRenderable' and 'I3D_mergeGroup' in obj) and (obj['I3D_nonRenderable'] and obj['I3D_mergeGroup'] == 0):
+            #     print("Multiple shapes defined!")
 
             if 'I3D_static' in obj and obj['I3D_static'] == 1 and not is_placeable:
-                print("RigidBody: {} is marked as static!".format(obj.name))
+                print(f"RigidBody: {obj.name} is marked as static!")
                 info_count += 1
 
             if 'I3D_nonRenderable' and 'I3D_mergeGroup' and 'I3D_clipDistance' and 'I3D_boundingVolume' in obj and obj['I3D_nonRenderable'] == 0 and obj['I3D_mergeGroup'] == 0 and obj['I3D_clipDistance'] == 0 and obj['I3D_boundingVolume'] == '':
-                print("ClipDistance: {} has no clip distance set. This causes performance issues!".format(obj.name))
+                print(f"ClipDistance: {obj.name} has no clip distance set. This causes performance issues!")
                 info_count += 1
 
             if 'decal' in obj.name.lower() and 'I3D_decalLayer' in obj and obj['I3D_decalLayer'] == 0:
-                print("DecalLayer: {}, has decal layer set to 0".format(obj.name))
+                print(f"DecalLayer: {obj.name}, has decal layer set to 0")
                 info_count += 1
 
             if ("decal" in obj.name.lower() == -1) and 'I3D_decalLayer' in obj and obj['I3D_decalLayer'] != 0:
@@ -82,24 +82,26 @@ class I3DEA_OT_verify_scene(bpy.types.Operator):
 
             if 'fillvolume' in obj.name.lower() != -1:
                 if 'I3D_cpuMesh' in obj and obj['I3D_cpuMesh'] == 0:
-                    print("FillVolume is not marked as CPU-Mesh!")
+                    print(f"FillVolume {obj.name} is not marked as CPU-Mesh!")
                     warning_count += 1
 
             if 'I3D_collision' in obj and obj['I3D_collision'] == 1:
-                if 'I3D_compound' in obj and obj['I3D_compound'] == 1:
-                    if "_main_component" not in obj.name:
-                        print("Component: Object {} is marked as compound, but name convention is wrong.".format(obj.name))
-                        warning_count += 1
-                if obj.scale != (1, 1, 1):
-                    print("Scale: collision {} is not scaled 1 1 1, apply scale.".format(obj.name))
+                if 'I3D_compound' in obj and obj['I3D_compound'] == 1 and 'I3D_trigger' in obj and obj['I3D_trigger'] == 0:
+                    if obj.parent is None:
+                        if "_main_component" not in obj.name:
+                            print(f"Component: Object {obj.name} is marked as compound, but name convention is wrong. Should be x_main_component")
+                            warning_count += 1
+
+                if obj.scale != Vector((1, 1, 1)):
+                    print(f"Scale: collision {obj.name} is not scaled 1 1 1, apply scale.")
                     warning_count += 1
 
             if 'effect' in obj.name.lower():
                 if not obj.data.color_attributes:
-                    print("EffectMesh: {}, is a effect but doesn't have a Vertex Color layer".format(obj.name))
+                    print(f"EffectMesh: {obj.name}, is a effect but doesn't have a Vertex Color layer")
                     warning_count += 1
                 if not len(obj.data.uv_layers) == 2:
-                    print("EffectMesh: {}, is a effect but doesn't have 2 UV layers".format(obj.name))
+                    print(f"EffectMesh: {obj.name}, is a effect but doesn't have 2 UV layers")
                     warning_count += 1
 
             for mo in obj.modifiers:
@@ -113,6 +115,7 @@ class I3DEA_OT_verify_scene(bpy.types.Operator):
         print("Errors:", error_count)
         print("Warnings:", warning_count)
         print("Info:", info_count)
+        # print("Poly count:", poly_count)
         return {'FINISHED'}
 
 
