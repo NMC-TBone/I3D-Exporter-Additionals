8c72b88119ce196a856e01228748760e20b2f8a4
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2024-11-28 00:55:08 +0100

    chore(release): add missing version in zip name

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,10 +3,9 @@
 # Replace version number in blender_manifest.toml with the new version number. Can contain dev version.
 sed -i "s/^version = \".*\"/version = \"$1\"/" $GITHUB_WORKSPACE/i3d_exporter_additionals/blender_manifest.toml
 
-# Split the version tag into separate numbers for potential use (if needed)
-IFS='.-' read -ra VERSION <<< "$1"
-
-# Zip the addon directory into a new build
-sudo apt-get install -y zip
+# Create a versioned zip file
 cd $GITHUB_WORKSPACE
-zip -r i3d_exporter_additionals.zip i3d_exporter_additionals
+zip -r "i3d_exporter_additionals-$1.zip" i3d_exporter_additionals
+
+echo "Zip file created: i3d_exporter_additionals-$1.zip"
+ls -la
